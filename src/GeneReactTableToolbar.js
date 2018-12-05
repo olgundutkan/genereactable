4c77b3dba78019ad7a41880441a9c39386f8066e
Olgun DUTKAN <olgundutkan@gmail.com>
2018-12-05 20:51:29 +0300

    Modified import dependency injections

--- src/GeneReactTableToolbar.js
+++ src/GeneReactTableToolbar.js
@@ -2,17 +2,8 @@ import React, { Component } from "react";
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import TableSortLabel from "@material-ui/core/TableSortLabel";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
 import DeleteIcon from "@material-ui/icons/Delete";
@@ -44,7 +35,7 @@ const styles = theme => ({
   }
 });
 
-class GeneReactTableToolbar extends React.Component {
+class GeneReactTableToolbar extends Component {
   createSortHandler = property => event => {
     this.props.onRequestSort(event, property);
   };
